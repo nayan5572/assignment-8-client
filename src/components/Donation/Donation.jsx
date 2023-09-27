a9d70618816012c294c647c820f794740d029a1b
nayan <nayan.dev572@gmail.com>
2023-09-27 20:09:35 +0600

    create and logic build see all button

--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -26,19 +26,19 @@ const Donation = () => {
         }
     }, [donation]);
 
+    // Button Condition
+    const showSeeAllButton = dataLength < item.length && item.length >= 4;
     return (
         <div>
             <h2>This is Donation: {item.length}</h2>
             <div className="grid grid-cols-2 gap-x-5">
-                {
-                    item.slice(0, dataLength).map(product => <Applied key={product.id} product={product}></Applied>)
-                }
+                {item.slice(0, dataLength).map(product => <Applied key={product.id} product={product} />)}
             </div>
-            <div className="mt-10 mb-4 text-center">
-                <div className={dataLength === item.length && 'hidden'}>
-                    <button onClick={() => setDataLength(dataLength.length)} className="bg-[#009444] py-4 px-7 rounded-lg font-semibold text-white">See All</button>
+            {showSeeAllButton ? (
+                <div className="mt-10 mb-4 text-center">
+                    <button onClick={() => setDataLength(item.length)} className="bg-[#009444] py-4 px-7 rounded-lg font-semibold text-white">See All</button>
                 </div>
-            </div>
+            ) : null}
         </div>
     );
 };
