ca87a4907a51ca935e7aaca3873b1770c1c8b151
nayan <nayan.dev572@gmail.com>
2023-09-27 23:46:34 +0600

    fixed some issue

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -14,14 +14,14 @@ const ProductDetails = () => {
     // css style tailwindCss
     const textColor = item.text_bg_color;
 
-   
-    const handleSweetAlert = ()=>{
+
+    const handleSweetAlert = () => {
         saveProductApplication(idInt);
         Swal.fire({
             title: 'Successfully Added!',
             icon: 'success',
             confirmButtonText: 'Ok'
-          });
+        });
     }
 
     return (
@@ -30,7 +30,7 @@ const ProductDetails = () => {
                 <img src={item.picture} alt="Image" className="w-[1320px] h-[700px] mx-auto" />
                 <div className="absolute inset-x-0 bottom-0 bg-black opacity-50 h-36 w-[1320px] mx-auto"></div>
                 <div className="absolute bottom-0 left-28 right-0 py-9">
-                    <button onClick={handleSweetAlert} style={{backgroundColor: textColor}} className="rounded-lg ml-9 py-4 px-6 text-white">Donate ${item.price}</button>
+                    <button onClick={handleSweetAlert} style={{ backgroundColor: textColor }} className="rounded-lg ml-9 py-4 px-6 text-white">Donate ${item.price}</button>
                 </div>
             </div>
             <h2 className="text-4xl font-bold mt-14">{item.category}</h2>
@@ -40,12 +40,3 @@ const ProductDetails = () => {
 };
 
 export default ProductDetails;
-
-
-{/* // <div>
-        //     <h2>Data: {dataData.length}</h2>
-        //     <div>
-        //         <img className="w-[1320px] h-[550px]" src={item.picture} alt="" />
-        //         <button className="btn">{item.price}</button>
-        //     </div>
-        // </div> */}
